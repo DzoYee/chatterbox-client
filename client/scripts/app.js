09c6744a703f6e5bd8f97b8989c918300be1e142
dzoyee <joeyleung8@gmail.com>
2015-12-08 14:01:03 -0800

    Implemented MakeRoom and Send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,7 +6,7 @@ var app = {
 app.server = "https://api.parse.com/1/classes/chatterbox";
 app.init = function(){
   $('.username').on('click',app.addFriend);
-  $('.submit').on('submit',app.handleSubmit);
+  $('.submit').on('click',app.handleSubmit());
   $('#select').on('change',app.roomToggle("here"));
   /*$('#main').append('<div class="username" onclick="app.addFriend()">placeholder</div>');
   $('#main').append('<form id="send"><input type="text"></input></form>');
@@ -15,15 +15,24 @@ app.init = function(){
 app.clearMessages = function(){
   $('#chats').children().remove();
 };
+
+app.sanitizeData = function(data) {
+  // input: array of objects
+
+  //return clean data
+};
+
 app.addMessage = function(messages){
-  //iterate through first 20 of array
+
   var hashRooms = {}
 
+  $('#chats').children().remove();
   _.each(messages, function(message){
     $('#chats').append('<div class="username" room="'+ message.roomname +'">' + message.objectId + ' : ' + message.username + ' : ' + message.text + '</div>');
     hashRooms[message.roomname] = message.roomname;
   })
 
+  $('roomSelection').children().remove();
   _.each(hashRooms, function(room) {
     $('select').append('<option value="'+room+'"">'+room+'</option>');
   })
@@ -35,12 +44,41 @@ app.addFriend = function(){
   
 };
 app.handleSubmit = function(){
+  var username = window.location.search.slice(10);
+  //pull roomname
+
+  //pull message
+  //('#message').val()
 
 };
-app.roomToggle = function(room) {
-  console.log('room: ', room);
+app.handleRoom = function(){
+  var message = new MakeMessage($('#newroom').val());
+  app.send(message);
+};
+
+app.handleMessage = function(){
+  var username = window.location.search.slice(10);
+  var roomname = $('#newroom').val();
+  var message = $('#message').val();
+  var message = new MakeMessage(roomname, username, message);
+  app.send(message);
+};
+
+
+
+var MakeMessage = function(room, username, message){
+  this.roomname = room;
+  this.username = username;
+  this.text = message;
+};
+
+
+
+app.roomToggle = function() {
+  var room = $( "#roomSelection" ).val();
   $('.username').toggle();
-  $('div[room='+room+']').toggle();
+  $('div[room="'+room+'"]').toggle();
+  $('#newroom').val(room);
 };
 
 app.fetch = function(){
@@ -55,7 +93,7 @@ app.fetch = function(){
       console.log('chatterbox: Message sent');
 
       var messageList = data.results
-        // console.log('message: ', message);
+      console.log('messageList: ', messageList);
       app.addMessage(messageList);
 
     },
@@ -65,7 +103,7 @@ app.fetch = function(){
     }
   });  
 };
-app.send = function(){
+app.send = function(message){
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -74,7 +112,7 @@ app.send = function(){
     contentType: 'application/json',
     success: function (data) {
       messages.push(data);
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message sent ', message);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -85,13 +123,13 @@ app.send = function(){
 
 
 
-var message = {
-          username: 'Mel Brooks',
-          text: 'It\'s good to be the king',
-          roomname: 'lobby'
-};
+ var message = {
+           username: 'Mel Brooks',
+           text: 'It\'s good to be the king',
+           roomname: 'lobby'
+ };
 
-// app.init()
+app.init()
  app.fetch()
 
 
